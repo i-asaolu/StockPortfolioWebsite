d0aad92dfd49947e3eeff17d95c16e45bd19cdc6
i-asaolu <ikpomwonsaa@gmail.com>
2024-06-21 06:25:19 -0400

    Changing from IEX Cloud Api to Alpha Vantage Api

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,49 +2,44 @@ from django.shortcuts import render, redirect
 from .models import Stock
 from .forms import StockForm
 from django.contrib import messages
-#Type in public key
-public_key = 
-# Create your views here.
+import requests
+import json
+
+# Replace 'public_key' with your Alpha Vantage API key
+public_key = 'MDWZ9JK2CGH7DOC2'
+
 def home(request):
-	import requests
-	import json
-
-	if request.method == 'POST':
-		ticker = request.POST['ticker']
-		api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+ticker+"/quote?token="+public_key)
-
-		try:
-			api = json.loads(api_request.content)
-		except Exception as e:
-			api = "Error..."
-		return render(request, 'home.html', {'api':api})
-	else:
-		return render(request, 'home.html', {'ticker':"Enter a ticker symbol above..."})
-
-	#pk_78e5c006aeef4f3ea34f21ae5ae4ce67
-	
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get(f"https://www.alphavantage.co/query?function=OVERVIEW&symbol={ticker}&apikey={public_key}")
+        try:
+            api = json.loads(api_request.content)
+            if not api:
+                api = "Error: No data found for the given ticker symbol."
+            else:
+                api = {k.lower(): v for k, v in api.items()}
+        except Exception as e:
+            api = "Error: An error occurred while fetching data."
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
 
 def about(request):
-	return render(request, 'about.html', {})
-
-
-
+    return render(request, 'about.html', {})
 
 def add_stock(request):
-
-	if request.method == 'POST':
-		form = StockForm(request.POST or None)
-
-		if form.is_valid():
-			form.save()
-			messages.success(request, ("Stock Has Been Added"))
-			return redirect('add_stock')
-	else:		
-		ticker = Stock.objects.all()
-		return render(request, 'add_stock.html', {'ticker':ticker})
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Stock Has Been Added")
+            return redirect('add_stock')
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
 
 def delete(request, stock_id):
-	item = Stock.objects.get(pk=stock_id)
-	item.delete()
-	messages.success(request, ("Stock Has Been Deleted!"))
-	return redirect(add_stock)
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, "Stock Has Been Deleted!")
+    return redirect('add_stock')
